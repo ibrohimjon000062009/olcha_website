3ad3d71f51b817ffe47865b69a5c3becb153cbf0
ibrohimjon000062009 <you@example.com>
2025-05-14 21:37:16 +0500

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Home from './components/Home/Home';
 
+
+
 function App() {
   return (
     <div className="wrp__one">
-      <Home />
+        
+        <Home/>
+      
     </div>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,6 +12,13 @@ function Home() {
   { id: 3,title: "Redmi 14C note", price: "3 550 000 so'm", old:"", month:"400 000 so'm x 12oy", image: phone3 },
   { id: 4, badge: "3%", title: "Huwaei X700", price: "4 500 000 so'm", old:"4 700 000 so'm", month:"500 000 so'm x 12oy", image: phone4 },
   { id: 5, title: "Huwaei X700", price: "1 752 000 so'm", month:"179 000 so'm x 12oy", image: phone5 },
+
+
+    { id: 1, badge: "7%", title: "Xiomi Redmi 14C", price: "3 250 000 so'm", old:"4 200 000 so'm", month:"415 000 so'm x 12oy", image: phone1 },
+  { id: 2, badge: "13%", title: "Poco X6", price: "5 550 000 so'm", old:"6 000 000 so'm", month:"575 000 so'm x 12oy", image:phone2 },
+  { id: 3,title: "Redmi 14C note", price: "3 550 000 so'm", old:"", month:"400 000 so'm x 12oy", image: phone3 },
+  { id: 4, badge: "3%", title: "Huwaei X700", price: "4 500 000 so'm", old:"4 700 000 so'm", month:"500 000 so'm x 12oy", image: phone4 },
+  { id: 5, title: "Huwaei X700", price: "1 752 000 so'm", month:"179 000 so'm x 12oy", image: phone5 },
   
 ]; 
     
@@ -24,4 +31,4 @@ function Home() {
   );
 }
 
-export default Home
+export default Home 
